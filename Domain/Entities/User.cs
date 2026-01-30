6fe04db9501c7442eaf024a5b2e116ea63f8d190
Ka666aH <dmitryhoroshev05@mail.ru>
2026-01-30 15:11:29 +0300

    Перенести сущности в папку Entities.

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,11 +1,11 @@
-﻿namespace Domain
+﻿namespace Domain.Entities
 {
     public class User
     {
         public string Login { get; init; }
         public string HashedPassword { get; private set; }
-        private List<Goal> _goals = [];
-        public IReadOnlyList<Goal> Goals => _goals.AsReadOnly();
+        //private List<Goal> _goals = [];
+        //public IReadOnlyList<Goal> Goals => _goals.AsReadOnly();
 #pragma warning disable CS8618 
         public User(string login, string hashedPassword)
         {
@@ -19,8 +19,8 @@
             if (string.IsNullOrWhiteSpace(hashedPassword)) throw new ArgumentNullException("Hashed password can't be empty.");
             HashedPassword = hashedPassword;
         }
-        public void AddGoal(Goal goal) => _goals.Add(goal);
-        public void RemoveGoal(Goal goal) => _goals.Remove(goal);
+        //public void AddGoal(Goal goal) => _goals.Add(goal);
+        //public void RemoveGoal(Goal goal) => _goals.Remove(goal);
 
         private User() { }
 #pragma warning restore CS8618 
