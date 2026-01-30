fb59efd1b169ddd6462fd81820f7a83dce1027ca
Ka666aH <dmitryhoroshev05@mail.ru>
2026-01-30 14:24:47 +0300

    Реализовать сущности.

---
+++ Domain/Goal.cs
@@ -0,0 +1,40 @@
+﻿namespace Domain
+{
+    public class Goal
+    {
+        public Guid Id { get; init; }
+        public string UserLogin { get; init; }
+        public User User { get; private set; }
+        public string Title { get; private set; }
+        public string? Description { get; private set; }
+        public DateTime CreateAt { get; init; }
+        public DateTime? Deadline { get; private set; }
+#pragma warning disable CS8618 
+        public Goal(string userLogin, string title, string? description, DateTime? deadline)
+        {
+            UserLogin = userLogin;
+            SetTitle(title);
+            SetDescription(description);
+            CreateAt = DateTime.Now;
+            SetDeadline(deadline);
+        }
+        public void SetTitle(string title)
+        {
+            if(string.IsNullOrWhiteSpace(title)) throw new ArgumentNullException("Title can't be empty.");
+            Title = title.Trim();
+        }
+        public void SetDescription(string? description)
+        {
+            if (string.IsNullOrWhiteSpace(description)) Description = null;
+            Description = description!.Trim();
+        }
+        public void SetDeadline(DateTime? deadline)
+        {
+            if (deadline <= CreateAt) throw new ArgumentException("Deadline already fucked up!");
+            Deadline = deadline;
+        }
+
+        private Goal() { }
+#pragma warning restore CS8618 
+    }
+}
---
+++ Domain/User.cs
@@ -0,0 +1,31 @@
+﻿namespace Domain
+{
+    public class User
+    {
+        public string Login { get; private set; }
+        public string HashedPassword { get; private set; }
+        private List<Goal> _goals = [];
+        public IReadOnlyList<Goal> Goals => _goals.AsReadOnly();
+#pragma warning disable CS8618 
+        public User(string login, string hashedPassword)
+        {
+            SetLogin(login);
+            SetHashedPassword(hashedPassword);
+        }
+        public void SetLogin(string login)
+        {
+            if(string.IsNullOrWhiteSpace(login)) throw new ArgumentNullException("Login can't be empty.");
+            Login = login.Trim();
+        }
+        public void SetHashedPassword(string hashedPassword)
+        {
+            if (string.IsNullOrWhiteSpace(hashedPassword)) throw new ArgumentNullException("Hashed password can't be empty.");
+            HashedPassword = hashedPassword;
+        }
+        public void AddGoal(Goal goal) => _goals.Add(goal);
+        public void RemoveGoal(Goal goal) => _goals.Remove(goal);
+
+        private User() { }
+#pragma warning restore CS8618 
+    }
+}
