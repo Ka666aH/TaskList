26f44ca262fcd52ce4c28ada92c6bd73c7a052de
Ka666aH <dmitryhoroshev05@mail.ru>
2026-01-30 15:11:02 +0300

    Реализовать интерфейсы репозиториев.

---
+++ Application/Interfaces/IGoalRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IGoalRepository
+    {
+        Task AddGoalAsync(string login, Goal goal, CancellationToken ct = default);
+        Task<Goal> GetGoalByIdAsync(Guid goalId, CancellationToken ct = default);
+        Task UpdateGoalAsync(Guid goalId, string newTitle, string? newDescription, DateTime? newDeadline, CancellationToken ct = default);
+        Task RemoveGoalAsync(string login, Goal goal, CancellationToken ct = default);
+    }
+}
---
+++ Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task SaveChangesAsync(CancellationToken ct = default);
+    }
+}
