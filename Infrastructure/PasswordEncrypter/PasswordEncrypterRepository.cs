b8ba4d8fdfabb9946c798e954f11d707e45f90de
Ka666aH <dmitryhoroshev05@mail.ru>
2026-02-02 14:33:51 +0300

    Реализовать репозиторий шифровальщика паролей.

---
+++ Infrastructure/PasswordEncrypter/PasswordEncrypterRepository.cs
@@ -0,0 +1,11 @@
+﻿using Application.Interfaces.RepositoryInterfaces;
+using BCrypt.Net;
+
+namespace Infrastructure.PasswordEncrypter
+{
+    internal class PasswordEncrypterRepository : IPasswordEncrypterRepository
+    {
+        public string Encrypt(string password) => BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+        public bool Verify(string password, string hashedPassword) => BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
+    }
+}
