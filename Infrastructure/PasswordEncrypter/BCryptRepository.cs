0f9341fd4928def1807ac29f56c409d7019412c4
Ka666aH <dmitryhoroshev05@mail.ru>
2026-02-02 15:50:50 +0300

    Переименовать репозиторий.

--- Infrastructure/PasswordEncrypter/BCryptRepository.cs
+++ Infrastructure/PasswordEncrypter/BCryptRepository.cs
@@ -3,7 +3,7 @@ using BCrypt.Net;
 
 namespace Infrastructure.PasswordEncrypter
 {
-    internal class PasswordEncrypterRepository : IPasswordEncrypterRepository
+    public class BCryptRepository : IPasswordEncrypterRepository
     {
         public string Encrypt(string password) => BCrypt.Net.BCrypt.EnhancedHashPassword(password);
         public bool Verify(string password, string hashedPassword) => BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
