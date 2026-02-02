5d4f08cf3126df4b0261047e9aca48791967c473
Ka666aH <dmitryhoroshev05@mail.ru>
2026-02-02 09:55:37 +0300

    Добавить пакет DotNetEnv. Подключить PostgreSQL.

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,11 +1,23 @@
-using Infrastucture.Database;
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 builder.Services.AddControllers();
 //// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
-builder.Services.AddDbContext<PostgreSQLDbContext>();
+
+Env.Load();
+var connectionString = 
+    $"Host={Environment.GetEnvironmentVariable("POSTGRESQL_HOST")};" +
+    $"Port={Environment.GetEnvironmentVariable("POSTGRESQL_PORT")};" +
+    $"Database={Environment.GetEnvironmentVariable("POSTGRESQL_DATABASE")};" +
+    $"Username={Environment.GetEnvironmentVariable("POSTGRESQL_USER")};" +
+    $"Password={Environment.GetEnvironmentVariable("POSTGRESQL_PASSWORD")}";
+builder.Services.AddDbContext<PostgreSQLDbContext>(options => options.UseNpgsql(connectionString));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
