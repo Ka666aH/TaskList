d6b9395d4e4cd2a7d97d6ca117e02646504cc187
Ka666aH <dmitryhoroshev05@mail.ru>
2026-02-02 15:52:07 +0300

    Реализовать репозиторий для работы с JWT токеном.

---
+++ Infrastructure/Token/JWTRepository.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces.RepositoryInterfaces;
+using Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace Infrastructure.Token
+{
+    public class JWTRepository : ITokenRepository
+    {
+        public string GenerateToken(User user)
+        {
+            var sc = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes("testkeytestkeytestkeytestkeytestkey")), SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: [new Claim("sub", user.Login)],
+                expires: DateTime.UtcNow.AddMinutes(1),
+                signingCredentials: sc
+                );
+
+            var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+            return tokenValue;
+        }
+    }
+}
