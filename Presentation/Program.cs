27945395339105b006b45a39cd584c905abe2656
Ka666aH <dmitryhoroshev05@mail.ru>
2026-02-03 12:56:28 +0300

    Подключить Swagger.

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -14,7 +14,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 //// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 Env.Load();
 var connectionString = 
     $"Host={Environment.GetEnvironmentVariable("POSTGRESQL_HOST")};" +
@@ -35,10 +36,13 @@ builder.Services.AddScoped<IAuthService,AuthService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
