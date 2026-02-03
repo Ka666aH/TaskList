f121ebe2cd8f4335abd5b91f134f066d04e62081
Ka666aH <dmitryhoroshev05@mail.ru>
2026-02-03 15:42:25 +0300

    Вынести логику работы с AuthCookie в CookieHelper.

---
+++ Infrastructure/Cookies/CookieHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Runtime.CompilerServices;
+
+namespace Infrastructure.Cookies
+{
+    public static class CookieHelper
+    {
+        public const string AuthCookieName = "jwt";
+        public static void SetAuthCookie(this HttpResponse response, string token)
+        {
+            response.Cookies.Append(AuthCookieName, token);
+        }
+        public static void DeleteAuthCookie(this HttpResponse response)
+        {
+            response.Cookies.Delete(AuthCookieName);
+        }
+    }
+}
