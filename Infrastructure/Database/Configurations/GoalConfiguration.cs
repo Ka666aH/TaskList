bc22720584b1cba65f5158f373b1488f2002935e
Ka666aH <dmitryhoroshev05@mail.ru>
2026-02-09 12:39:54 +0300

    Добавить индекс по UserLogin в таблицу Goals.

--- Infrastructure/Database/Configurations/GoalConfiguration.cs
+++ Infrastructure/Database/Configurations/GoalConfiguration.cs
@@ -19,6 +19,8 @@ namespace Infrastructure.Database.Configurations
                     .WithMany(u => u.Goals)
                     .HasForeignKey(g => g.UserLogin)
                     .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasIndex(g => g.UserLogin);
         }
     }
 }
---
+++ Infrastructure/Migrations/20260209093158_add-userlogin-index.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class adduserloginindex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Goal_Users_UserLogin",
+                table: "Goal");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Goal",
+                table: "Goal");
+
+            migrationBuilder.RenameTable(
+                name: "Goal",
+                newName: "Goals");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Goal_UserLogin",
+                table: "Goals",
+                newName: "IX_Goals_UserLogin");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Goals",
+                table: "Goals",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Goals_Users_UserLogin",
+                table: "Goals",
+                column: "UserLogin",
+                principalTable: "Users",
+                principalColumn: "Login",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Goals_Users_UserLogin",
+                table: "Goals");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Goals",
+                table: "Goals");
+
+            migrationBuilder.RenameTable(
+                name: "Goals",
+                newName: "Goal");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Goals_UserLogin",
+                table: "Goal",
+                newName: "IX_Goal_UserLogin");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Goal",
+                table: "Goal",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Goal_Users_UserLogin",
+                table: "Goal",
+                column: "UserLogin",
+                principalTable: "Users",
+                principalColumn: "Login",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
