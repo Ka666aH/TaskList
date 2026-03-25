a0f23ded90923aa58666a6dc508d84f892ab8a06
Ka666aH <dmitryhoroshev05@mail.ru>
2026-03-25 08:23:54 +0300

    Реализовать тесты контроллера задач.

---
+++ TaskList.Tests/GoalControllerTests.cs
@@ -0,0 +1,248 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using Presentation.DTO;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace TaskList.Tests
+{
+    [Collection("Integration")]
+    public class GoalControllerTests : GoalControllerTestsBase
+    {
+        [Theory]
+        [MemberData(nameof(GetAddGoalValidData), MemberType = typeof(GoalControllerTests))]
+        public async Task Add_goal_pass(string? description, int? hoursToDeadline)
+        {
+            //Arrange
+            string login = "user";
+            await RegisterAndLoginUser(login: login);
+
+            var deadline = hoursToDeadline != null
+                ? DateTime.UtcNow.AddHours((double)hoursToDeadline)
+                : (DateTime?)null;
+            //Act
+            var addGoalResopnse = await AddGoal("title", description, deadline);
+            //Assert
+            addGoalResopnse.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            var goals = await FindUserGoals(login);
+            goals.Count.Should().Be(1);
+            var goal = goals[0];
+            goal.Title.Should().Be("title");
+            goal.Description.Should().Be(description);
+            if (deadline.HasValue) goal.Deadline.Should().BeCloseTo(deadline.Value, 1.Seconds());
+            else goal.Deadline.Should().BeNull();
+            goal.UserLogin.Should().Be(login);
+            goal.CreateAt.Should().BeCloseTo(DateTime.UtcNow, 5.Seconds());
+
+        }
+        public static TheoryData<string?, int?> GetAddGoalValidData()
+        {
+            return new TheoryData<string?, int?>
+            {
+                { "description", 7*24 },
+                { "", 1 },
+                //{ "", null },
+                { null, null }
+            };
+        }
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("   ")]
+        public async Task Add_goal_fails_when_title_is_empty(string title)
+        {
+            //Arrange
+            string login = "user";
+            await RegisterAndLoginUser(login: login);
+            //Act
+            var addGoalResopnse = await AddGoal(title, "description", DateTime.UtcNow.AddDays(7));
+            //Assert
+            addGoalResopnse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var goals = await FindUserGoals(login);
+            goals.Count.Should().Be(0);
+        }
+        [Fact]
+        public async Task Add_goal_fails_when_deadline_expired()
+        {
+            //Arrange
+            string login = "user";
+            await RegisterAndLoginUser(login: login);
+            //Act
+            var addGoalResopnse = await AddGoal("title", "description", DateTime.UtcNow.AddDays(-1));
+            //Assert
+            addGoalResopnse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var goals = await FindUserGoals(login);
+            goals.Count.Should().Be(0);
+        }
+        [Fact]
+        public async Task Get_user_goals_pass()
+        {
+            //Assert
+            string login = "user";
+            await RegisterAndLoginUser(login: login);
+            await AddGoal("title", "description", DateTime.UtcNow.AddDays(7));
+            await AddGoal("title2", "description2", DateTime.UtcNow.AddDays(7));
+            //Act
+            var getGoalsResponse = await GetGoals();
+            //Arrange
+            getGoalsResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var responseGoals = await getGoalsResponse.Content.ReadFromJsonAsync<List<GoalResponse>>(TestContext.Current.CancellationToken);
+            responseGoals.Should().NotBeNull();
+            responseGoals.Count.Should().Be(2);
+            responseGoals.Should().Contain(g => g.Title == "title" && g.Description == "description")
+                .And.Contain(g => g.Title == "title2" && g.Description == "description2");
+
+            var dbGoals = await FindUserGoals(login);
+            dbGoals.Should().NotBeNull();
+            dbGoals.Count.Should().Be(2);
+            dbGoals.Should()
+                .Contain(g => g.Title == "title")
+                .And.Contain(g => g.Title == "title2");
+        }
+        [Fact]
+        public async Task Get_user_goal_pass()
+        {
+            //Arrange
+            string login = "user";
+            await RegisterAndLoginUser(login: login);
+            var addGoalResponse = await AddGoal("title", "description", DateTime.UtcNow.AddDays(7));
+            var goalId = addGoalResponse.Headers.Location!.ToString().Split('/').Last();
+            //Act
+            var getGoalResponse = await GetGoal(goalId);
+            //Assert
+            getGoalResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var responseGoal = await getGoalResponse.Content.ReadFromJsonAsync<GoalResponse>(TestContext.Current.CancellationToken);
+            responseGoal.Should().NotBeNull();
+            responseGoal.Id.ToString().Should().Be(goalId);
+            //responseGoal.Title.Should().Be("title");
+            //responseGoal.Description.Should().Be("description");
+        }
+        [Fact]
+        public async Task Get_user_goal_fails_when_goal_is_not_exist()
+        {
+            //Arrange
+            string login = "user";
+            await RegisterAndLoginUser(login: login);
+            //Act
+            var getGoalResponse = await GetGoal(Guid.NewGuid().ToString());
+            //Assert
+            getGoalResponse.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+        [Fact]
+        public async Task Delete_goal_pass()
+        {
+            //Arrange
+            string login = "user";
+            await RegisterAndLoginUser(login: login);
+            var addGoalResponse = await AddGoal("title", "description", DateTime.UtcNow.AddDays(7));
+            var goalId = addGoalResponse.Headers.Location!.ToString().Split('/').Last();
+            //Act
+            var deleteGoalResponse = await DeleteGoal(goalId);
+            //Assert
+            deleteGoalResponse.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+            var goals = await FindUserGoals(login);
+            goals.Should().BeEmpty();
+        }
+        [Fact]
+        public async Task Delete_goal_fails_when_goal_is_not_exist()
+        {
+            //Arrange
+            string login = "user";
+            await RegisterAndLoginUser(login: login);
+            //Act
+            var deleteGoalResponse = await DeleteGoal(Guid.NewGuid().ToString());
+            //Assert
+            deleteGoalResponse.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+        [Fact]
+        public async Task Update_goal_pass()
+        {
+            //Arrange
+            string login = "user";
+            await RegisterAndLoginUser(login: login);
+            var addGoalResponse = await AddGoal("title", "description", DateTime.UtcNow.AddDays(7));
+            var goalId = addGoalResponse.Headers.Location!.ToString().Split('/').Last();
+            var newDeadline = DateTime.UtcNow.AddMonths(1);
+            var goalRequest = new GoalRequest("new title", "new description", newDeadline);
+            //Act
+            var updateGoalResponse = await UpdateGoal(goalId, goalRequest);
+            //Assert
+            updateGoalResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var responseGoal = await updateGoalResponse.Content.ReadFromJsonAsync<GoalResponse>(TestContext.Current.CancellationToken);
+            responseGoal.Should().NotBeNull();
+            //responseGoal.Id.ToString().Should().Be(goalId); // Id = 00000000-0000-0000-0000-000000000000
+            responseGoal.Title.Should().Be("new title");
+            responseGoal.Description.Should().Be("new description");
+            responseGoal.Deadline.Should().Be(newDeadline);
+
+            var dbGoals = await FindUserGoals(login);
+            dbGoals.Count.Should().Be(1);
+            var dbGoal = dbGoals.First();
+            dbGoal.Id.ToString().Should().Be(goalId);
+            dbGoal.Title.Should().Be("new title");
+            dbGoal.Description.Should().Be("new description");
+            dbGoal.Deadline.Should().BeCloseTo(newDeadline, 1.Milliseconds());
+        }
+        [Fact]
+        public async Task Update_goal_fails_when_goal_is_not_exist()
+        {
+            //Arrange
+            string login = "user";
+            await RegisterAndLoginUser(login: login);
+            var newDeadline = DateTime.UtcNow.AddMonths(1);
+            var goalRequest = new GoalRequest("new title", "new description", newDeadline);
+            //Act
+            var updateGoalResponse = await UpdateGoal(Guid.NewGuid().ToString(), goalRequest);
+            //Assert
+            updateGoalResponse.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            var dbGoals = await FindUserGoals(login);
+            dbGoals.Should().BeEmpty();
+        }
+        [Fact]
+        public async Task Update_goal_fails_when_new_title_is_empty()
+        {
+            //Arrange
+            string login = "user";
+            await RegisterAndLoginUser(login: login);
+            var deadline = DateTime.UtcNow.AddDays(7);
+            var addGoalResponse = await AddGoal("title", "description", deadline);
+            var goalId = addGoalResponse.Headers.Location!.ToString().Split('/').Last();
+            var goalRequest = new GoalRequest("", "description", deadline);
+            //Act
+            var updateGoalResponse = await UpdateGoal(goalId, goalRequest);
+            //Assert
+            updateGoalResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+            var dbGoals = await FindUserGoals(login);
+            dbGoals.Count.Should().Be(1);
+            var dbGoal = dbGoals.First();
+            dbGoal.Id.ToString().Should().Be(goalId);
+            dbGoal.Title.Should().Be("title");
+        }
+        [Fact]
+        public async Task Update_goal_fails_when_new_deadline_is_expired()
+        {
+            //Arrange
+            string login = "user";
+            await RegisterAndLoginUser(login: login);
+            var oldDeadline = DateTime.UtcNow.AddDays(7);
+            var addGoalResponse = await AddGoal("title", "description", oldDeadline);
+            var goalId = addGoalResponse.Headers.Location!.ToString().Split('/').Last();
+            var newDeadline = DateTime.UtcNow.AddMonths(-1);
+            var goalRequest = new GoalRequest("title", "description", newDeadline);
+            //Act
+            var updateGoalResponse = await UpdateGoal(goalId, goalRequest);
+            //Assert
+            updateGoalResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+            var dbGoals = await FindUserGoals(login);
+            dbGoals.Count.Should().Be(1);
+            var dbGoal = dbGoals.First();
+            dbGoal.Id.ToString().Should().Be(goalId);
+            dbGoal.Deadline.Should().BeCloseTo(oldDeadline, 1.Milliseconds());
+        }
+    }
+}
---
+++ TaskList.Tests/GoalControllerTestsBase.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Presentation.DTO;
+using System.Net.Http.Json;
+
+namespace TaskList.Tests
+{
+    public class GoalControllerTestsBase : IntergrationTestsBase
+    {
+        protected async Task RegisterAndLoginUser(string login = "user", string password = "password")
+        {
+            await Register(login, password);
+            await LogIn(login, password);
+        }
+        protected async Task<HttpResponseMessage> AddGoal(string title, string? description, DateTime? deadline) =>
+            await _httpClient.PostAsJsonAsync("/goals", new GoalRequest(title, description, deadline), TestContext.Current.CancellationToken);
+        protected async Task<HttpResponseMessage> GetGoals() =>
+            await _httpClient.GetAsync("/goals", TestContext.Current.CancellationToken);
+        protected async Task<HttpResponseMessage> GetGoal(string goalId) =>
+            await _httpClient.GetAsync($"/goals/{goalId}", TestContext.Current.CancellationToken);
+        protected async Task<HttpResponseMessage> DeleteGoal(string goalId) =>
+            await _httpClient.DeleteAsync($"/goals/{goalId}", TestContext.Current.CancellationToken);
+        protected async Task<HttpResponseMessage> UpdateGoal(string goalId, GoalRequest goalRequest) =>
+            await _httpClient.PutAsJsonAsync($"/goals/{goalId}", goalRequest, TestContext.Current.CancellationToken);
+
+        protected async Task<IReadOnlyList<Goal>> FindUserGoals(string login)
+        {
+            var user = await _db.Users
+                .AsNoTracking()
+                .Include(u => u.Goals)
+                .FirstAsync(u => u.Login == login, TestContext.Current.CancellationToken);
+            return user.Goals;
+        }
+    }
+}
