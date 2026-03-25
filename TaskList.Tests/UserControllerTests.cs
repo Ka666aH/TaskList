7443c289890edd1f939e938a988deda5ce4f74fb
Ka666aH <dmitryhoroshev05@mail.ru>
2026-03-25 08:24:32 +0300

    Реализовать тесты контроллера пользователя.

---
+++ TaskList.Tests/UserControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using System.Net;
+namespace TaskList.Tests
+{
+    [Collection("Integration")]
+    public class UserControllerTests : UserControllerTestsBase
+    {
+        [Fact]
+        public async Task Delete_account_pass()
+        {
+            //Arrange
+            var login = "user";
+            await CreateAndAutorizeClient(login);
+            //Act
+            var deleteResponse = await DeleteAccount();
+            //Assert
+            deleteResponse.StatusCode.Should().Be(HttpStatusCode.NoContent);
+            (await FindUserByLogin(login)).Should().BeNull();
+            (await CountUsersInDatabase()).Should().Be(1);
+        }
+        [Fact]
+        public async Task Delete_account_fails_when_it_is_default_admin()
+        {
+            //Arrange
+            string login = "admin";
+            string password = "admin";
+            await LogIn(login, password);
+            //Act
+            var deleteResponse = await DeleteAccount();
+            //Assert
+            deleteResponse.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+            (await FindUserByLogin(login)).Should().NotBeNull();
+            (await CountUsersInDatabase()).Should().Be(1);
+        }
+        [Fact]
+        public async Task Change_password_pass()
+        {
+            //Arrange
+            var login = "user";
+            var password = "password";
+            var newPassword = "newpassword";
+            await CreateAndAutorizeClient(login, password);
+
+            //Act
+            var changeResponse = await ChangePassword(newPassword);
+
+            //Assert
+            changeResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var user = await FindUserByLogin(login);
+            user.Should().NotBeNull();
+            VerifyPassword(newPassword, user.HashedPassword).Should().BeTrue();
+        }
+        [Fact]
+        public async Task Change_password_fails_when_new_password_is_the_same_as_old()
+        {
+            //Arrange
+            var login = "user";
+            var password = "password";
+            await CreateAndAutorizeClient(login, password);
+
+            //Act
+            var changeResponse = await ChangePassword(password);
+
+            //Assert
+            changeResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
---
+++ TaskList.Tests/UserControllerTestsBase.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces.RepositoryInterfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Presentation.DTO;
+using System.Net.Http.Json;
+
+namespace TaskList.Tests
+{
+    public class UserControllerTestsBase : IntergrationTestsBase
+    {
+        protected IPasswordEncrypterRepository _passwordEncrypter =>
+            _scope.ServiceProvider.GetRequiredService<IPasswordEncrypterRepository>();
+
+        protected bool VerifyPassword(string password, string hashedPassword) => _passwordEncrypter.Verify(password, hashedPassword);
+
+        protected async Task<HttpResponseMessage> DeleteAccount() =>
+            await _httpClient.DeleteAsync("/account", TestContext.Current.CancellationToken);
+        protected async Task<HttpResponseMessage> ChangePassword(string newPassword) =>
+            await _httpClient.PatchAsJsonAsync("/account/password", new ChangePasswordRequest(newPassword), TestContext.Current.CancellationToken);
+    }
+}
