fe371ad7b6495801c8d02a1d61e80fb8a1d791b8
Ka666aH <dmitryhoroshev05@mail.ru>
2026-03-25 14:45:23 +0300

    Реализовать тесты контроллера прогноза погоды.

---
+++ TaskList.Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace TaskList.Tests
+{
+    public class WeatherForecastControllerTests : IntergrationTestsBase
+    {
+        [Fact]
+        public async Task Unauthorized_user_get_weather_forecast_pass()
+        {
+            //Act
+            var getWeatherForecastResponse = await _httpClient.GetAsync("/WeatherForecast", TestContext.Current.CancellationToken);
+            //Assert
+            getWeatherForecastResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var forecasts = await getWeatherForecastResponse.Content.ReadFromJsonAsync<List<WeatherForecast>>(TestContext.Current.CancellationToken);
+            forecasts.Should().NotBeNull();
+            forecasts.Count.Should().Be(5);
+            forecasts.Should().OnlyContain(f =>
+            f.Date >= DateOnly.FromDateTime(DateTime.Now) && f.Date <= DateOnly.FromDateTime(DateTime.Now.AddDays(5)) &&
+            f.TemperatureC >= -20 && f.TemperatureC < 55 &&
+            !string.IsNullOrEmpty(f.Summary)
+            );
+        }
+        [Fact]
+        public async Task Authorized_user_get_weather_forecast_pass()
+        {
+            //Act
+            await RegisterAndLogInClient();
+            var getWeatherForecastResponse = await _httpClient.GetAsync("/WeatherForecast", TestContext.Current.CancellationToken);
+            //Assert
+            getWeatherForecastResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var forecasts = await getWeatherForecastResponse.Content.ReadFromJsonAsync<List<WeatherForecast>>(TestContext.Current.CancellationToken);
+            forecasts.Should().NotBeNull();
+            forecasts.Count.Should().Be(5);
+            forecasts.Should().OnlyContain(f =>
+            f.Date >= DateOnly.FromDateTime(DateTime.Now) && f.Date <= DateOnly.FromDateTime(DateTime.Now.AddDays(5)) &&
+            f.TemperatureC >= -20 && f.TemperatureC < 55 &&
+            !string.IsNullOrEmpty(f.Summary)
+            );
+        }
+    }
+}
