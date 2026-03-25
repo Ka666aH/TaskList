11a3e8c62b6b1227d63242ab6600e4c04cdc6178
Ka666aH <dmitryhoroshev05@mail.ru>
2026-03-25 14:17:15 +0300

    Реализовать тесты контроллера администратора.

---
+++ TaskList.Tests/AdminControllerTests.cs
@@ -0,0 +1,85 @@
+﻿using Domain.Constants;
+using FluentAssertions;
+using System.Net;
+namespace TaskList.Tests
+{
+    public class AdminControllerTests : AdminControllerTestsBase
+    {
+        [Fact]
+        public async Task Change_role_from_client_to_admin_pass()
+        {
+            //Arrange
+            var login = "user";
+            await Register(login: login);
+            await LogIn("admin", "admin");
+            //Act
+            var changeRoleResponse = await ChangeRole(login, RoleType.Admin);
+            //Assert
+            changeRoleResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var user = await FindUserByLogin(login);
+            user.Should().NotBeNull();
+            user.RoleId.Should().Be((int)RoleType.Admin);
+        }
+        [Fact]
+        public async Task Change_role_from_admin_to_client_pass()
+        {
+            //Arrange
+            var login = "user";
+            await Register(login: login);
+            await LogIn("admin", "admin");
+            await ChangeRole(login, RoleType.Admin);
+            //Act
+            var changeRoleResponse = await ChangeRole(login, RoleType.Client);
+            //Assert
+            changeRoleResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var user = await FindUserByLogin(login);
+            user.Should().NotBeNull();
+            user.RoleId.Should().Be((int)RoleType.Client);
+        }
+        [Fact]
+        public async Task Change_role_from_default_admin_to_client_fails()
+        {
+            //Arrange
+            var login = "admin";
+            await LogIn("admin", "admin");
+            //Act
+            var changeRoleResponse = await ChangeRole(login, RoleType.Client);
+            //Assert
+            changeRoleResponse.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+            var user = await FindUserByLogin(login);
+            user.Should().NotBeNull();
+            user.RoleId.Should().Be((int)RoleType.Admin);
+        }
+        [Fact]
+        public async Task Change_role_from_client_to_client_pass()
+        {
+            //Arrange
+            var login = "user";
+            await Register(login);
+            await LogIn("admin", "admin");
+            //Act
+            var changeRoleResponse = await ChangeRole(login, RoleType.Client);
+            //Assert
+            changeRoleResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var user = await FindUserByLogin(login);
+            user.Should().NotBeNull();
+            user.RoleId.Should().Be((int)RoleType.Client);
+        }
+        [Fact]
+        public async Task Change_role_from_admin_to_admin_pass()
+        {
+            //Arrange
+            var login = "user";
+            await Register(login);
+            await LogIn("admin", "admin");
+            await ChangeRole(login, RoleType.Admin);
+            //Act
+            var changeRoleResponse = await ChangeRole(login, RoleType.Admin);
+            //Assert
+            changeRoleResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var user = await FindUserByLogin(login);
+            user.Should().NotBeNull();
+            user.RoleId.Should().Be((int)RoleType.Admin);
+        }
+    }
+}
---
+++ TaskList.Tests/AdminControllerTestsBase.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Constants;
+using Presentation.DTO;
+using System.Net.Http.Json;
+
+namespace TaskList.Tests
+{
+    public class AdminControllerTestsBase : IntergrationTestsBase
+    {
+        protected async Task<HttpResponseMessage> ChangeRole(string login, RoleType role) =>
+            await _httpClient.PatchAsJsonAsync($"admin/users/{login}/role", new ChangeRoleRequest(role.ToString()), TestContext.Current.CancellationToken);
+    }
+}
