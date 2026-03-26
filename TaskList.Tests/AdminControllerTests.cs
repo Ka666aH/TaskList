7356d2650b8027916c107b5356d1cfe1c5b4fc7b
Ka666aH <dmitryhoroshev05@mail.ru>
2026-03-26 08:20:18 +0300

    Реализовать тесты отчётности в контроллере администратора.

--- TaskList.Tests/AdminControllerTests.cs
+++ TaskList.Tests/AdminControllerTests.cs
@@ -1,6 +1,9 @@
 ﻿using Domain.Constants;
+using Domain.Entities;
 using FluentAssertions;
+using Presentation.DTO;
 using System.Net;
+using System.Net.Http.Json;
 namespace TaskList.Tests
 {
     [Collection("Integration")]
@@ -81,6 +84,99 @@ namespace TaskList.Tests
             var user = await FindUserByLogin(login);
             user.Should().NotBeNull();
             user.RoleId.Should().Be((int)RoleType.Admin);
+        }
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        public async Task Get_users_amount_pass(int amount)
+        {
+            //Arrange
+            for (int i = 0; i < amount; i++)
+            {
+                await Register($"user{i}");
+            }
+            await LogIn("admin", "admin");
+            //Act
+            var getUsersAmountResponse = await GetUsersAmount();
+            //Assert
+            getUsersAmountResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var usersAmount = await getUsersAmountResponse.Content.ReadFromJsonAsync<UsersAmountResponse>(TestContext.Current.CancellationToken);
+            usersAmount.Should().NotBeNull();
+            usersAmount.Amount.Should().Be(++amount);
+        }
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        public async Task Get_goals_amount_pass(int amount)
+        {
+            //Arrange
+            await RegisterAndLogInClient();
+            var tasks = Enumerable.Range(0, amount).Select(_ => AddGoal());
+            await Task.WhenAll(tasks);
+            await LogOut();
+            await LogIn("admin", "admin");
+            await AddGoal();
+            //Act
+            var getGoalsAmountResponse = await GetGoalsAmount();
+            //Assert
+            getGoalsAmountResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var goalsAmount = await getGoalsAmountResponse.Content.ReadFromJsonAsync<GoalsAmountResponse>(TestContext.Current.CancellationToken);
+            goalsAmount.Should().NotBeNull();
+            goalsAmount.Amount.Should().Be(++amount);
+        }
+        [Theory]
+        [MemberData(nameof(GetUsersPageData))]
+        public async Task Get_users_page_pass(int amount, int pageSize, int pageNumber, List<UserResponse> expected)
+        {
+            //Arrange
+            for (int i = 0; i < amount; i++)
+            {
+                await Register($"user{i}");
+            }
+            await LogIn("admin", "admin");
+            //Act
+            var getUsersPageResponse = await GetUsersPage(pageSize, pageNumber);
+            //Assert
+            getUsersPageResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var usersPage = await getUsersPageResponse.Content.ReadFromJsonAsync<List<UserResponse>>(TestContext.Current.CancellationToken);
+            usersPage.Should().NotBeNull();
+            usersPage.Should().BeEqualTo(expected);
+        }
+        public static TheoryData<int, int, int, List<UserResponse>> GetUsersPageData()
+        {
+            return new TheoryData<int, int, int, List<UserResponse>>
+            {
+                {0,0,0, [new UserResponse("admin")] },
+                {1,0,0, [new UserResponse("admin"), new UserResponse("user0")] },
+                {1,1,1, [new UserResponse("admin")] },
+                {1,1,2, [new UserResponse("user0")] },
+                {3,2,1, [new UserResponse("admin"), new UserResponse("user0")] },
+                {3,2,2, [new UserResponse("user1"), new UserResponse("user2")] },
+                {3,2,3, [] },
+            };
+        }
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        public async Task Get_user_goals_amount_pass(int amount)
+        {
+            //Arrange
+            string login = "user";
+            await RegisterAndLogInClient(login: login);
+            var tasks = Enumerable.Range(0, amount).Select(_ => AddGoal());
+            await Task.WhenAll(tasks);
+            await LogOut();
+            await LogIn("admin", "admin");
+            //Act
+            var getUserGoalsAmountResponse = await GetUserGoalsAmount(login);
+            //Assert
+            getUserGoalsAmountResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            var goalsAmount = await getUserGoalsAmountResponse.Content.ReadFromJsonAsync<GoalsAmountResponse>(TestContext.Current.CancellationToken);
+            goalsAmount.Should().NotBeNull();
+            goalsAmount.Amount.Should().Be(amount);
         }
     }
 }
--- TaskList.Tests/AdminControllerTestsBase.cs
+++ TaskList.Tests/AdminControllerTestsBase.cs
@@ -6,7 +6,18 @@ namespace TaskList.Tests
 {
     public class AdminControllerTestsBase : IntergrationTestsBase
     {
+        protected async Task<HttpResponseMessage> AddGoal(string title = "title", string? description = "description", DateTime? deadline = null) =>
+            await _httpClient.PostAsJsonAsync("/goals", new GoalRequest(title, description, deadline), TestContext.Current.CancellationToken);
         protected async Task<HttpResponseMessage> ChangeRole(string login, RoleType role) =>
             await _httpClient.PatchAsJsonAsync($"admin/users/{login}/role", new ChangeRoleRequest(role.ToString()), TestContext.Current.CancellationToken);
+        protected async Task<HttpResponseMessage> GetUsersAmount() =>
+            await _httpClient.GetAsync($"admin/users/amount/", TestContext.Current.CancellationToken);
+        protected async Task<HttpResponseMessage> GetGoalsAmount() =>
+            await _httpClient.GetAsync($"admin/goals/amount/", TestContext.Current.CancellationToken);
+        protected async Task<HttpResponseMessage> GetUsersPage(int pageSize, int page) =>
+            await _httpClient.GetAsync($"/admin/users?pageSize={pageSize}&page={page}", TestContext.Current.CancellationToken);
+        protected async Task<HttpResponseMessage> GetUserGoalsAmount(string login) =>
+            await _httpClient.GetAsync($"/goals/amount/{login}", TestContext.Current.CancellationToken);
+
     }
 }
